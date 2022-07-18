2b7fff41ba96e795a1fca300f81fa1dfe5b5c824
Eric Mueller <eric@hollyburn.com>
2022-07-18 11:52:45 -0700

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         filename: "index.js",
         path: path.resolve(__dirname, 'dist'),
         library: 'hollyburn-Lib',
-        libraryTarget: "umb"
+        libraryTarget: "umd"
     },
     module: {
         rules: [
