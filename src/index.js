addfe653ceccb02eb7b1517b276674749dc96330
Eric Mueller <eric@hollyburn.com>
2022-07-18 15:05:36 -0700

    added header.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles/index.css";
 import {QualifyForm} from "./components/qualify-form";
+import {Header} from "./components/header";
 
 export {
-    QualifyForm,
+    QualifyForm, Header
 }
