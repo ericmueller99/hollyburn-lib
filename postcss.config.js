81bfeef905cba143a58c5889fe2d64c5016b2025
Eric Mueller <eric@hollyburn.com>
2022-07-18 16:27:34 -0700

    added header.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,4 @@
 const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    }
+    plugins: ['postcss-preset-env', tailwindcss]
 }
